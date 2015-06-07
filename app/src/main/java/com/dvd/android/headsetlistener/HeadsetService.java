97bee0b8802a5f71b2de37d24b01f20d6c94aaa2
DVDAndroid <davide.intelligent@gmail.com>
2015-06-07 15:40:42 +0200

    notification on service started/stopped

--- app/src/main/java/com/dvd/android/headsetlistener/HeadsetService.java
+++ app/src/main/java/com/dvd/android/headsetlistener/HeadsetService.java
@@ -1,11 +1,52 @@
 package com.dvd.android.headsetlistener;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Handler;
 import android.os.IBinder;
 
 public class HeadsetService extends Service {
+
+	private int NOTIFICATION_ID = 22;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		createNotification(true);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		createNotification(false);
+	}
+
+	private void createNotification(boolean created) {
+		int id = created ? R.string.service_started : R.string.service_stopped;
+
+		final NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		notificationManager.cancel(22);
+
+		Notification notification = new Notification.Builder(this)
+				.setContentTitle(getString(R.string.app_name))
+				.setContentText(getString(id)).setAutoCancel(true)
+				.setSmallIcon(R.mipmap.ic_launcher)
+				.setWhen(System.currentTimeMillis()).build();
+
+		notification.flags |= Notification.FLAG_AUTO_CANCEL;
+		notificationManager.notify(NOTIFICATION_ID, notification);
+
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				notificationManager.cancel(NOTIFICATION_ID);
+			}
+		}, 4500);
+	}
 
 	@Override
 	public IBinder onBind(Intent arg0) {
